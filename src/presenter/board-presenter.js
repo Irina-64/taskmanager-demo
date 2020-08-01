909f9c858a77fe9d198c0898421ed2ee2686b84b
Sasha Sushko <sushko@outlook.com>
2020-08-02 00:26:21 +0300

    3.2.2 Добавит новый компонент - заглушку
    - Создаем новый компонент - заглушку
    - По условию - нет задач - рендерим заглушку на страницу
    - Если задачи есть, то показываем компонент сортировки
    
    N.B. Чтобы проверить, что заглушка показывается, обнулите константу TASK_COUNT

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -4,6 +4,7 @@ import TaskListView from '../view/task-list-view.js';
 import TaskView from '../view/task-view.js';
 import TaskEditView from '../view/task-edit-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
+import NoTaskView from '../view/no-task-view.js';
 import {render} from '../render.js';
 
 const TASK_COUNT_PER_STEP = 8;
@@ -23,32 +24,37 @@ export default class BoardPresenter {
     this.#boardTasks = [...this.#tasksModel.tasks];
 
     render(this.#boardComponent, this.#boardContainer);
-    render(new SortView(), this.#boardComponent.element);
-    render(this.#taskListComponent, this.#boardComponent.element);
 
-    for (let i = 0; i < Math.min(this.#boardTasks.length, TASK_COUNT_PER_STEP); i++) {
-      this.#renderTask(this.#boardTasks[i]);
-    }
+    if (this.#boardTasks.every((task) => task.isArchive)) {
+      render(new NoTaskView(), this.#boardComponent.element);
+    } else {
+      render(new SortView(), this.#boardComponent.element);
+      render(this.#taskListComponent, this.#boardComponent.element);
 
-    if (this.#boardTasks.length > TASK_COUNT_PER_STEP) {
-      const loadMoreButtonComponent = new LoadMoreButtonView();
-      render(loadMoreButtonComponent, this.#boardComponent.element);
+      for (let i = 0; i < Math.min(this.#boardTasks.length, TASK_COUNT_PER_STEP); i++) {
+        this.#renderTask(this.#boardTasks[i]);
+      }
 
-      let renderedTaskCount = TASK_COUNT_PER_STEP;
+      if (this.#boardTasks.length > TASK_COUNT_PER_STEP) {
+        const loadMoreButtonComponent = new LoadMoreButtonView();
+        render(loadMoreButtonComponent, this.#boardComponent.element);
 
-      loadMoreButtonComponent.element.addEventListener('click', (evt) => {
-        evt.preventDefault();
-        this.#boardTasks
-          .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-          .forEach((task) => this.#renderTask(task));
+        let renderedTaskCount = TASK_COUNT_PER_STEP;
 
-        renderedTaskCount += TASK_COUNT_PER_STEP;
+        loadMoreButtonComponent.element.addEventListener('click', (evt) => {
+          evt.preventDefault();
+          this.#boardTasks
+            .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+            .forEach((task) => this.#renderTask(task));
 
-        if (renderedTaskCount >= this.#boardTasks.length) {
-          loadMoreButtonComponent.element.remove();
-          loadMoreButtonComponent.removeElement();
-        }
-      });
+          renderedTaskCount += TASK_COUNT_PER_STEP;
+
+          if (renderedTaskCount >= this.#boardTasks.length) {
+            loadMoreButtonComponent.element.remove();
+            loadMoreButtonComponent.removeElement();
+          }
+        });
+      }
     }
   }
 
