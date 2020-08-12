a92ebaf50ece135fef9d3d08d768d6b8e2e97040
Sasha Sushko <sushko@outlook.com>
2020-08-12 23:22:22 +0300

    6.1.4 Добавит внутренние обработчики
    - Объявим два обработчика: #dueDateToggleHandler и #repeatingToggleHandler
    - Навесим их на элементы, пока в конструкторе
    
    Если запустить дев-сервер, открыть форму редактирования и попробовать переключить выбор даты или повторения, первый раз всё получится. А потом - нет. Дело в том, что во время перерисовки обработчики удаляются

--- src/view/task-edit-view.js
+++ src/view/task-edit-view.js
@@ -139,6 +139,11 @@ export default class TaskEditView extends AbstractView {
   constructor(task = BLANK_TASK) {
     super();
     this._state = TaskEditView.parseTaskToState(task);
+
+    this.element.querySelector('.card__date-deadline-toggle')
+      .addEventListener('click', this.#dueDateToggleHandler);
+    this.element.querySelector('.card__repeat-toggle')
+      .addEventListener('click', this.#repeatingToggleHandler);
   }
 
   get template() {
@@ -173,6 +178,20 @@ export default class TaskEditView extends AbstractView {
     const newElement = this.element;
 
     parent.replaceChild(newElement, prevElement);
+  }
+
+  #dueDateToggleHandler = (evt) => {
+    evt.preventDefault();
+    this.updateElement({
+      isDueDate: !this._state.isDueDate,
+    });
+  }
+
+  #repeatingToggleHandler = (evt) => {
+    evt.preventDefault();
+    this.updateElement({
+      isRepeating: !this._state.isRepeating,
+    });
   }
 
   static parseTaskToState = (task) => ({...task,
