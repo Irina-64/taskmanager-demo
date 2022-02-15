5fd5ab04640643b72ae6b6527fafa8dbf73d0f07
Lydia Zakharova <jahontis@gmail.com>
2022-02-15 15:20:32 +0300

    1.7 Добавит презентер доски
    - Создадим отдельный модуль для работы с доской
    - Перенесём код, отвечающий за отрисовку доски, из main.js в BoardPresenter
    - Импортируем BoardPresenter в точку входа и вызовем его метод init()

--- src/main.js
+++ src/main.js
@@ -1,33 +1,15 @@
 import NewTaskButtonView from './view/new-task-button-view.js';
 import FilterView from './view/filter-view.js';
-import BoardView from './view/board-view.js';
-import SortView from './view/sort-view.js';
-import TaskListView from './view/task-list-view.js';
-import TaskView from './view/task-view.js';
-import TaskEditView from './view/task-edit-view.js';
-import LoadMoreButtonView from './view/load-more-button-view.js';
 import {render} from './render.js';
+import BoardPresenter from './presenter/board-presenter.js';
 
 const TASK_COUNT = 3;
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const boardPresenter = new BoardPresenter();
 
 render(new NewTaskButtonView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
 
-const boardComponent = new BoardView();
-render(boardComponent, siteMainElement);
-
-render(new SortView(), boardComponent.getElement());
-
-const taskListComponent = new TaskListView();
-render(taskListComponent, boardComponent.getElement());
-
-render(new TaskEditView(), taskListComponent.getElement());
-
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(new TaskView(), taskListComponent.getElement());
-}
-
-render(new LoadMoreButtonView(), boardComponent.getElement());
+boardPresenter.init(siteMainElement, TASK_COUNT);
